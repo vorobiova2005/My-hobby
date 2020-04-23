41b137138da16d3528ab67778ad71fd1f4dfbdc7
vorobiova2005 <vorobyova.valentina2005@gmail.com>
2020-04-23 11:28:27 +0300

    Add main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+var name = prompt("Hi, what's your name?");
+
+if ( Symbol >= "6", name > Symbol ) {
+alert("Hello," + name);
+
+} else  {
+    Symbol < "6", name < Symbol
+alert("Good morning," + name);
+}
